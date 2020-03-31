8c75f1d77a03acc182c4dbc9782b468f377d3917
ajourd <61282481+ajourd@users.noreply.github.com>
2020-03-31 11:04:47 +0200

    Add files via upload

--- connexion.ads
+++ connexion.ads
@@ -1,6 +1,6 @@
 package Connexion is 
    Subtype T_Mot is String (1 .. 15);
-   Subtype T_Login is String (1..41);
+   Subtype T_Login is String (1..31);
    subtype T_Mdp is String (1..10);
    type T_Profil is (Directeur,Secretaire,Charge_Etude,Testeuse);
    type T_Personne is record
